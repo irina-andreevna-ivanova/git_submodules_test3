5e023644eae5bfa034a0b218b73a539499390e8f
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-09-11 17:31:56 +0300

    Initial commit

---
+++ core_module/src/a/b/c/DummyClass1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 9/11/2018.
+ * Project: git_submodules_test3
+ * *******************************
+ */
+public class DummyClass1 {
+}
